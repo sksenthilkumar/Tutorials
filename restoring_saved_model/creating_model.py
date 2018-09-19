23d6d0321598d7b9f8b16885987d06f39c641db4
sksenthilkumar <sksenthilkumar93@gmail.com>
2018-09-19 18:54:33 +0200

    changing the method of creating a session

--- restoring_saved_model/creating_model.py
+++ restoring_saved_model/creating_model.py
@@ -6,11 +6,7 @@ from tensorflow.examples.tutorials.mnist import input_data
 
 
 # Read data
-if os.path.exists("MNIST_data/"):
-    # Import images from repository
-    print("Importing mnist dataset from repository")
-else:
-    mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 image_size = 28
 labels_size = 10
@@ -40,24 +36,25 @@ correct_prediction = tf.equal(tf.argmax(output, 1), tf.argmax(labels, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 # Run the training
-sess = tf.InteractiveSession()
-sess.run(tf.global_variables_initializer())
+# sess = tf.InteractiveSession()
 
 
-for i in range(steps_number):
-  # Get the next batch
-  input_batch, labels_batch = mnist.train.next_batch(batch_size)
-  feed_dict = {training_data: input_batch, labels: labels_batch}
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    for i in range(steps_number):
+      # Get the next batch
+      input_batch, labels_batch = mnist.train.next_batch(batch_size)
+      feed_dict = {training_data: input_batch, labels: labels_batch}
 
-  # Run the training step
-  train_step.run(feed_dict=feed_dict)
+      # Run the training step
+      train_step.run(feed_dict=feed_dict)
 
-  # Print the accuracy progress on the batch every 100 steps
-  if i%100 == 0:
-    train_accuracy = accuracy.eval(feed_dict=feed_dict)
-    print("Step %d, training batch accuracy %g %%"%(i, train_accuracy*100))
+      # Print the accuracy progress on the batch every 100 steps
+      if i%100 == 0:
+        train_accuracy = accuracy.eval(feed_dict=feed_dict)
+        print("Step %d, training batch accuracy %g %%"%(i, train_accuracy*100))
 
-# Evaluate on the test set
-test_accuracy = accuracy.eval(feed_dict={training_data: mnist.test.images, labels: mnist.test.labels})
-print("Test accuracy: %g %%"%(test_accuracy*100))
+    # Evaluate on the test set
+    test_accuracy = accuracy.eval(feed_dict={training_data: mnist.test.images, labels: mnist.test.labels})
+    print("Test accuracy: %g %%"%(test_accuracy*100))
 
